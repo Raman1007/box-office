d05e371327390338b4f581c6cb8332f210e12d34
Raman <ramansonker900@gmail.com>
2021-07-14 17:38:06 +0530

    created two new more hooks:useLastQuery and useShow

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -7,10 +7,10 @@ import ShowGrid from '../components/show/ShowGrid';
 const Starred = () => {
   const [starred] = useShows();
 
-  const [shows, setShows] =useState(null);
+  const [shows, setShows] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-   useEffect(() => {
+  useEffect(() => {
     if (starred && starred.length > 0) {
       const promises = starred.map(showId => apiGet(`/shows/${showId}`));
       Promise.all(promises)
